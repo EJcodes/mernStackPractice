29bfe2bec469913c19aab13810411e9491ab1c9a
EJcodes <ihdemon.exe@gmail.com>
2019-10-17 19:59:45 -0400

    I don't know why my commits are not be registered on github

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,5 +1,7 @@
 import Product from '../../models/Product'
 
+// testing and trying to fix these ghost commits
+
 export default async (req, res) => {
     const { _id } = req.query
     const product = await Product.findOne({ _id })
