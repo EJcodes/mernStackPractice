a63326751914537fa98b2655cccd80b0c1570fd8
EJcodes <ihdemon@gmail.com>
2019-10-17 19:55:06 -0400

    testing commit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b4211996cde8dc2fdf33";
+/******/ 	var hotCurrentHash = "7ad7a5c22e89aaec71b1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
