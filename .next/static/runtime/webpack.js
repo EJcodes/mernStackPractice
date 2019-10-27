7d4ea17158d60e44879e5e1203fec6e9ac16f88e
EJcodes <IHMDemon.exe@gmail.com>
2019-10-27 16:26:25 -0400

    had to console log to see if i was recieving the unique id from mongoDB product schema

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a71d8323a0373edfc5d";
+/******/ 	var hotCurrentHash = "22832ba5d3ad7663efcb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,16 @@ import axios from "axios";
 import ProductList from '../components/Index/ProductList'
 
 function Home({ products }) {
+  console.log(products);
   return <ProductList products={products}/>
 }
 
 Home.getInitialProps = async () => {
 //fetch data on server 
+const url = "http://localhost:3000/api/products";
+const response = await axios.get(url);
+return { products: response.data};
 //return response data as an object
-  const url = "http://localhost:3000/api/products";
-  const response = await axios.get(url);
-  return { products: response.data};
 // note: this object will be merged with existing props 
 }
 
