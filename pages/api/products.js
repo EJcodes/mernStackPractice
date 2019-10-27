ea19c95c76da52f46fb555ae2d028f72b9497795
EJcodes <IHMDemon.exe@gmail.com>
2019-10-27 16:18:23 -0400

    switched the source of data from the mock data to the mongodb origin

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,12 @@
-import products from "../../static/products.json";
+// import products from "../../static/products.json";
+import Product from '../../models/Product'
 import connectDb from "../../utils/connectDb";
 //why isn't my commits showing on github?????
 
 
 connectDb();
 
-export default (req, res) => {
+export default async (req, res) => {
+  const products = await Product.find()
   res.status(200).json(products);
 };
