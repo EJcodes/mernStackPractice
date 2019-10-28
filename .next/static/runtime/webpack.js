ff63409d7aeedb18afa188a3a8a2fa6154ff04af
EJcodes <IHMDemon.exe@gmail.com>
2019-10-28 15:26:08 -0400

    turn the url into a turnary so it knows if it needs to be deployment or local enviroment

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "edbebf16f0b0c7258762";
+/******/ 	var hotCurrentHash = "e30582be627c5d350732";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
