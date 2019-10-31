a6ea341b01f9ae0ab7134e1c77f8ee0bf780352c
EJcodes <IHMDemon.exe@gmail.com>
2019-10-30 23:02:55 -0400

    finally error free for axios post request successfully posted on cloudinary api item is visible on the item list on merchandise page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ea6c274244ef29e3905c";
+/******/ 	var hotCurrentHash = "baac1cc662059cb16ed2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
