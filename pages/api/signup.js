f98c8ba724cecb43a277241e13485f5db2ba8acd
EJcodes <IHMDemon.exe@gmail.com>
2019-11-06 23:22:12 -0500

    added the apporiate changes in signup.js for the cart to work properly

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,5 +1,6 @@
 import connectDb from '../../utils/connectDb';
 import User from '../../models/User';
+import Cart from '../../models/Cart';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import isEmail from 'validator/lib/isEmail';
@@ -36,9 +37,11 @@ export default async (req, res) => {
             password: hash
         }).save()
         console.log({newUser})
-        // 4) create a token for the new user
+        // 5)  Create a cart for the new user
+        await new Cart({ user: newUser._id }).save;
+        // 6) create a token for the new user
          const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '7d'});
-        // 5) send back token 
+        // 7) send back token 
         res.status(201).json(token);
     } catch (error)  {
         console.error(error)
