96b103bd4a0aca567a43f6b4c9a0a73e02dfffa5
EJcodes <IHMDemon.exe@gmail.com>
2019-11-16 22:31:55 -0500

    adding logic so only admin can see create link and delete button under products

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -5,9 +5,12 @@ import baseUrl from '../../utils/baseUrl';
 import { useRouter } from 'next/router'
 
 
-function ProductAttributes({ description, _id }) {
+function ProductAttributes({ description, _id, user }) {
   const [modal, setModal] = React.useState(false);
-  const router = useRouter()
+  const router = useRouter();
+  const isRoot = user && user.role === "root";
+  const isAdmin = user && user.role === "admin";
+  const isRootOrAdmin = isRoot || isAdmin;
 
   async function handleDelete() {
     const url = `${baseUrl}/api/product`
@@ -20,6 +23,8 @@ function ProductAttributes({ description, _id }) {
     <>
       <Header as="h3">About this product</Header>
       <p>{ description }</p>
+      { isRootOrAdmin && (
+      <>
       <Button 
         icon="trash alternate outline"
         color="red"
@@ -36,8 +41,9 @@ function ProductAttributes({ description, _id }) {
           <Button negative icon="trash" labelPosition="right" content="Delete" onClick={handleDelete}/>
           
         </Modal.Actions>
-
       </Modal>
+      </>
+      )}
     </>
   );
 }
