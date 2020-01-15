29caa3437f47c9fccad0088009c5edfaa2dc2cec
EJcodes <IHMDemon.exe@gmail.com>
2020-01-15 16:09:40 -0500

    working on stripe connectivitiy

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -7,5 +7,14 @@ import calculateCartTotal from '../../utils/calculateCartTotal';
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
 export default async (req,res) => {
-    const 
+    const  { paymentData } = req.body;
+
+    try {
+        // 1) Verify and get user id from token
+        // 2) Find cart based on user id, populate it
+        // 3) Calculate cart totals again from cart products
+        // 4) Get email for payment data, see if email is linked w/ existing
+    } catch (error) {
+        
+    }
 }
